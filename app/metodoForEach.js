c60b168100c15241f1af15a0f896176b7235b233
Sullara <datausquelara@hotmail.com>
2025-12-12 19:34:11 -0300

    aula 2

---
+++ app/metodoForEach.js
@@ -0,0 +1,20 @@
+const ElementoParaInserirLivros = document.getElementById("livros");
+
+function exibirOsLivrosNaTela(livrosComDesconto){
+    livrosComDesconto.forEach(livro => {
+        elementoParaInserirLivros.innerHTML +=`
+            <div class="livro">
+                <img class="livro__imagens " src="${livro.imagem}"
+                alt="${livro.alt}" />
+                <h2 class="livro__titulo">
+                ${livro.titulo}
+                </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+                <div class="tags">
+                    <span class="tag">Front-end</span>
+                </div>
+            </div>
+        `
+    })
+}
